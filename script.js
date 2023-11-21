8b893d7d5780ded85cc830732bba3efde944f4cf
Daniela <daniela.scholz93@gmail.com>
2023-11-21 18:24:08 +0100

    added imprint, privacy-policy

--- script.js
+++ script.js
@@ -17,6 +17,21 @@ function removeClass(elem, className) {
 }
 
 
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+
 async function init() {
     let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     let response = await fetch(url);
